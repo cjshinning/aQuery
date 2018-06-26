41727c445383eb1788a72ac8c23a8e006fb061d1
cjshinning <784319417@qq.com>
2018-06-26 20:37:17 +0800

    jquery征途架构

---
+++ js/aquery.js
@@ -0,0 +1,24 @@
+;(function(global,factory){
+	console.log(global)
+	factory(global);
+}(typeof window !== 'undefined' ? window : this, function(window, noGlobal){
+	var jQuery = function( selector, context ) {
+		return new jQuery.fn.init( selector, context );
+	};
+	jQuery.fn = jQuery.prototype = {};
+	// 核心方法
+	// 回调系统
+	// 异步队列
+	// 数据缓存
+	// 队列操作
+	// 选择器引
+	// 属性操作
+	// 节点遍历
+	// 文档处理
+	// 样式操作
+	// 属性操作
+	// 事件体系
+	// AJAX交互
+	// 动画引擎
+	return jQuery;
+}))
